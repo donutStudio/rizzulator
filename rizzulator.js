de35c2866ef96f8b6a4751a283a2f31a1ad656b6
donutStudio <79951682+donutStudio@users.noreply.github.com>
2023-04-13 20:16:04 -0400

    Update rizzulator.js

--- rizzulator.js
+++ rizzulator.js
@@ -1,16 +1,14 @@
-var rizzwords =["L Rizz","W Rizz","Less Rizz Than Nikocado","Rizzard of Oz","Chrizztiano Rizznaldo","Sped Monkey Get Out of Here","Can Only Rizz Up Animals","Idiot","No Rizz", "L Bozo", "Sped Monk","Rizz Anyone Up","Does Not Pull","Save Some For The Rest Of Us","Rizzly Bear"]
-
-var answer = rizzwords[Math.floor(Math.random()*rizzwords.length)];
+var rizzwords = ["L Rizz", "W Rizz", "Less Rizz Than Nikocado", "Rizzard of Oz", "Chrizztiano Rizznaldo", "Sped Monkey Get Out of Here", "Can Only Rizz Up Animals", "Idiot", "No Rizz", "L Bozo", "Sped Monk", "Rizz Anyone Up", "Does Not Pull", "Save Some For The Rest Of Us", "Rizzly Bear"];
 
 function Calc() {
-    document.getElementById("output").innerHTML = answer ;
+  var elapsedSeconds = Math.floor((Date.now() - window.performance.timing.navigationStart) / 1000);
+  var index = Math.min(elapsedSeconds, rizzwords.length - 1);
+  document.getElementById("output").innerHTML = rizzwords[index];
 }
 
 function Output() {
-    alert("Hi");
-    document.getElementById("startCalc").onclick = Calc ;
+  alert("Hi");
+  document.getElementById("startCalc").onclick = Calc;
 }
 
 Output();
-
-
